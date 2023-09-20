07c401a42eee76264fffb04f398741d043fac217
mahyarmoghadaam <Mahyar.Moghadam@outlook.com>
2023-09-20 12:08:05 +0330

    feat(version-0.1.0): implement version 0.1.0 sample

---
+++ app/src/main/java/com/example/gizo/simple/Application.kt
@@ -0,0 +1,84 @@
+package com.example.gizo.simple
+
+import android.app.Application
+import android.util.Log
+import androidx.camera.video.Quality
+import de.artificient.gizo.sdk.Gizo
+import de.artificient.gizo.sdk.model.AnalysisDelegateType
+import de.artificient.gizo.sdk.model.FileLocationPath
+import de.artificient.gizo.sdk.setting.*
+
+class Application : Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+        Gizo.initialize(
+            this,
+            GizoAppOptions.Builder("GIZO_LICENSE")
+                .debug(true)
+                .folderName("GizoSample")
+                .analysisSetting(GizoAnalysisSettings.Builder()
+                    .allowAnalysis(true)
+                    .modelName("arti_sense.data")
+                    .loadDelegate(AnalysisDelegateType.Auto)
+                    .collisionThreshold(0.5f)
+                    .tailgatingThreshold(1.0f)
+                    .saveTtcCsvFile(true)
+                    .ttcFileLocation(FileLocationPath.CACHE)
+                    .saveDataTimerPeriod(10L)
+                    .saveMatrixFile(true)
+                    .matrixFileLocation(FileLocationPath.CACHE)
+                    .build()
+                )
+                .gpsSetting(
+                    GizoGpsSetting.Builder()
+                        .allowGps(true)
+                        .mapBoxKey("MAPBOX_PUBLIC_KEY")
+                        .interval(1000L)
+                        .maxWaitTime(1000L)
+                        .withForegroundService(true)
+                        .saveCsvFile(true)
+                        .fileLocation(FileLocationPath.CACHE)
+                        .saveDataTimerPeriod(10L)
+                        .build()
+                )
+                .imuSetting(
+                    GizoImuSetting.Builder()
+                        .allowAccelerationSensor(true)
+                        .allowMagneticSensor(true)
+                        .allowGyroscopeSensor(true)
+                        .saveCsvFile(true)
+                        .fileLocation(FileLocationPath.CACHE)
+                        .saveDataTimerPeriod(10L)
+                        .build()
+                )
+                .videoSetting(
+                    GizoVideoSetting.Builder()
+                        .allowRecording(true)
+                        .quality(Quality.HD)
+                        .fileLocation(FileLocationPath.CACHE)
+                        .build()
+                )
+                .batterySetting(
+                    GizoBatterySetting.Builder()
+                        .checkBattery(true)
+                        .lowBatteryLimit(25f)
+                        .lowBatteryStop(15f)
+                        .interval(5000L)
+                        .build()
+                )
+                .orientationSetting(
+                    GizoOrientationSetting.Builder()
+                        .allowGravitySensor(true)
+                        .build()
+                )
+                .build()
+        )
+
+        Gizo.app.setLoadModelObserver { status ->
+            Log.d("LoadModelStatus", "status:" + status.name)
+        }
+
+        Gizo.app.loadModel()
+    }
+}
